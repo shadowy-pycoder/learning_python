90911959415d72a7a41618033542ab491a169a84
witcher-sense <35629483+witcher-sense@users.noreply.github.com>
2022-09-26 21:43:54 +0300

    Add files via upload

--- password_generator.py
+++ password_generator.py
@@ -5,8 +5,8 @@ if len(sys.argv) < 3:
     print('Usage app.py <size_password> <weak/medium/strong>')
     sys.exit ( 1 )
 
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
+size_password = int(sys.argv[1])
+type_password = sys.argv[2]
 
 a = {
     '1': '!@#$%^&*()-+?_=,<>/:;.',
@@ -18,29 +18,25 @@ password = ''
 a_temp = ''
 
 #size_password = int(input("Size of password: "))
-size_password = int(arg1)
 
 
 while True:
     #type_password = input("Weak, Medium or Strong password? ").lower()
-    #if type_password == "weak":
-    if arg2 == 'weak':
+    if type_password == "weak":
         while size_password:
             a_temp = choice(a['2'])
             password += choice(a_temp)
             size_password -= 1
         print(password)
         break
-    #elif type_password == "medium":
-    elif arg2 == 'medium':
+    elif type_password == "medium":
         while size_password:
             a_temp = choice((a['2'], a['3']))
             password += choice(a_temp)
             size_password -= 1
         print(password)
         break
-    #elif type_password == "strong":
-    elif arg2 == 'strong':
+    elif type_password == "strong":
         while size_password:
             a_temp = choice((a['1'], a['2'], a['3']))
             password += choice(a_temp)
