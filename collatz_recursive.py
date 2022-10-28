af12c9af026ec6ec8e20590d327ce251f54f431f
witcher-sense <35629483+witcher-sense@users.noreply.github.com>
2022-10-28 11:53:14 +0300

    Update collatz_recursive.py

--- collatz_recursive.py
+++ collatz_recursive.py
@@ -2,16 +2,14 @@
 # Write a function named collatz() that has one parameter named number. If
 # number is even, then collatz() should print number // 2 and return this value.
 
-def collatz(number=None, array=[]) -> list:
+def collatz(number=None) -> list:
     if number == None or number <= 0:
         return [None]
-    array = array[:] + [number]
     if number == 1:
-        return array
+        return [number]
     if number % 2 == 0:
-        return collatz(number := number // 2, array)
-    return collatz(number := 3 * number + 1, array)
-
+        return [number] + collatz(number // 2)
+    return [number] + collatz(3 * number + 1)
 
 print(collatz(15) == [15, 46, 23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1])
 print(collatz() == [None])
