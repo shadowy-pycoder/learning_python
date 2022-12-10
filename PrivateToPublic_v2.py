728addaaad084736c1240dabb0241f8a92e0ec8e
witcher-sense <35629483+witcher-sense@users.noreply.github.com>
2022-12-10 23:34:32 +0600

    Update PrivateToPublic_v2.py

--- PrivateToPublic_v2.py
+++ PrivateToPublic_v2.py
@@ -36,6 +36,7 @@ class PublicKey:
                 'Private key must be a hexadecimal number')
         if key <= 0 or key >= self.n_curve:
             raise Exception("Invalid Scalar/Private Key")
+
         self.__private_key = key
 
     def __modinv(self, a, n):
@@ -112,15 +113,18 @@ class PublicKey:
 
         return private_wif, private_wif_comp
 
-    def print_private_keys_wif(self):
+    def print_private_keys(self):
         """Printing private keys in WIF"""
         priv_key, priv_key_comp = self.__convert_private_to_wif()
+        priv_hex = f'0x{hex(self.private_key)[2:]:0>64}'
+        print(f"\nHEX - private key\n{priv_hex}")
+        print(f"Length: {len(priv_hex)}\n")
         print(f"\nWIF - private key\n{priv_key}")
         print(f"Length: {len(priv_key)}\n")
         print(f"WIF compressed - private key\n{priv_key_comp}")
         print(f"Length: {len(priv_key_comp)}\n")
 
-    def print_public_key(self):
+    def print_public_keys(self):
         """Printing public key in different formats"""
         public_key = self.__compute_public()
         print("\nThe uncompressed public key (not address):")
@@ -149,10 +153,9 @@ class PublicKey:
 prompt = input(f"Please insert your private key in HEX format (0x): ")
 
 my_key = PublicKey(prompt)
-my_key.print_public_key()
-my_key.print_private_keys_wif()
-for i in range(10, 16):
+my_key.print_public_keys()
+my_key.print_private_keys()
+for i in range(11, 16):
     my_key.private_key = hex(i)
-    print(f'0x{hex(my_key.private_key)[2:]:0>64}')
-    my_key.print_public_key()
-    my_key.print_private_keys_wif()
+    my_key.print_public_keys()
+    my_key.print_private_keys()
