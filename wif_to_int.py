ddd5dca46d84d4b1920867cf7dfda184b2f37249
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2023-01-02 12:30:27 +0600

    Create wif_to_int.py

---
+++ wif_to_int.py
@@ -0,0 +1,23 @@
+import base58
+
+from sha256 import sha256
+
+
+def wif_to_bytes(wif: str) -> tuple[bytes, ...]:
+    private_key = base58.b58decode(wif)
+    return private_key[:1], private_key[1:-4], private_key[-4:]
+
+
+def is_checksum_valid(version: bytes, private_key: bytes, checksum: bytes) -> bool:
+    checksum_temp = sha256(sha256(version + private_key))[:4]
+    return checksum_temp == checksum
+
+
+def wif_to_int_private_key(wif: str) -> int:
+    # https://en.bitcoin.it/wiki/Wallet_import_format
+    version, private_key, checksum = wif_to_bytes(wif)
+    if not is_checksum_valid(version, private_key, checksum):
+        raise ValueError("Invalid WIF checksum")
+    private_key_int = int.from_bytes(private_key[:-1], 'big') if len(
+        private_key) == 33 else int.from_bytes(private_key, 'big')
+    return private_key_int
