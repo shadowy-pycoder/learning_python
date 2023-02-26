70a9cd7e10306db776cc7571cbbed33c295c8f8d
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2023-02-26 16:04:52 +0300

    Create flask_password_validator.py

---
+++ flask_password_validator.py
@@ -0,0 +1,14 @@
+def validate_password(self, password):
+        message = 'Password should contain at least '
+        error_log = []
+        errors = {
+            '1 digit': re.search(r'\d', password.data) is None,
+            '1 uppercase letter': re.search(r'[A-Z]', password.data) is None,
+            '1 lowercase letter': re.search(r'[a-z]', password.data) is None,
+            '1 special character': re.search(r'\W', password.data) is None,
+        }
+        for err_msg, error in errors.items():
+            if error:
+                error_log.append(err_msg)
+        if error_log:
+            raise ValidationError(message + ', '.join(err for err in error_log))
